ccddd727597482d904951c73e9f9359801b1b3de
Junhwan Beom <solkblte@icloud.com>
2018-09-08 19:54:51 +0900

    yes.go

---
+++ README.md
@@ -0,0 +1,16 @@
+# CYES
+
+A colorful yes.
+
+## Install & Build
+```shell
+go get -u github.com/JedBeom/cyes
+go install github.com/JedBeom/cyes
+export PATH=$PATH:$HOME/go/bin
+```
+
+## Run
+```shell
+cyes yes
+```
+
---
+++ yes.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/fatih/color"
+	"os"
+)
+
+func main() {
+	var msg string
+	if len(os.Args) == 1 {
+		msg = "y\n"
+	} else {
+		msg = os.Args[1] + "\n"
+	}
+	print := os.Stdout
+	red := color.New(color.FgRed).FprintFunc()
+	blue := color.New(color.FgBlue).FprintFunc()
+	green := color.New(color.FgGreen).FprintFunc()
+	yellow := color.New(color.FgYellow).FprintFunc()
+	magenta := color.New(color.FgMagenta).FprintFunc()
+	cyan := color.New(color.FgCyan).FprintFunc()
+	white := color.New(color.FgWhite).FprintFunc()
+
+	for {
+		red(print, msg)
+		blue(print, msg)
+		green(print, msg)
+		yellow(print, msg)
+		magenta(print, msg)
+		cyan(print, msg)
+		white(print, msg)
+	}
+}
